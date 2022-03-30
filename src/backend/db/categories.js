0e580fd8d139ec564285fba79585c31b746057c2
bipul <itsmebipul00@gmail.com>
2022-03-30 14:51:31 +0530

    mokbee ---setup

---
+++ src/backend/db/categories.js
@@ -0,0 +1,45 @@
+import { v4 as uuid } from 'uuid'
+
+/**
+ * Category Database can be added here.
+ * You can add category of your wish with different attributes
+ * */
+
+export const categories = [
+	{
+		_id: uuid(),
+		categoryName: 'children',
+	},
+	{
+		_id: uuid(),
+		categoryName: 'action',
+	},
+	{
+		_id: uuid(),
+		categoryName: 'comedy',
+	},
+	{
+		_id: uuid(),
+		categoryName: 'drama',
+	},
+	{
+		_id: uuid(),
+		categoryName: 'fantasy',
+	},
+	{
+		_id: uuid(),
+		categoryName: 'horror',
+	},
+	{
+		_id: uuid(),
+		categoryName: 'mystery',
+	},
+	{
+		_id: uuid(),
+		categoryName: 'romance',
+	},
+	{
+		_id: uuid(),
+		categoryName: 'thriller',
+	},
+]
